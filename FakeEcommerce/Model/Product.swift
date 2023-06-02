843c8dc1acc3a47392e7b88fb54f81f63024d705
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2023-06-02 14:23:42 +0530

    create enum class for category type

--- FakeEcommerce/Model/Product.swift
+++ FakeEcommerce/Model/Product.swift
@@ -12,6 +12,12 @@ struct Product: Identifiable{
     var title: String
     var price: Double
     var description: String
-    var category: String
+    var category: CategoryType
     var image: String
+}
+enum CategoryType: String, CaseIterable{
+    case electronics = "electronics"
+    case jewelery = "jewelery"
+    case men = "men's clothing"
+    case women = "women's clothing"
 }
