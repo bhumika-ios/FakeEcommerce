a44019b83fdb14aa831bd11a50a60297cb150765
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2023-06-02 13:31:15 +0530

    Initial Commit

---
+++ FakeEcommerce/FakeEcommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FakeEcommerceApp.swift
+//  FakeEcommerce
+//
+//  Created by Bhumika Patel on 02/06/23.
+//
+
+import SwiftUI
+
+@main
+struct FakeEcommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
